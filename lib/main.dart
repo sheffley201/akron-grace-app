967e0774b4bbfaba6220ae151347afec9933da52
Spencer Heffley <spheffley@yahoo.com>
2022-04-19 08:37:37 -0400

    Changed scrolling to customscrollview. Added SliverAppBar in place of AppBar.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         title: 'Akron Grace EC',//use MaterialApp() widget like this
         home: Home() //create new widget class for this 'home' to
       // escape 'No MediaQuery widget found' error
@@ -25,8 +25,32 @@ class MyApp extends StatelessWidget{
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  List <Widget> widgets = const [
+    //WelcomeWidget(),
+    LatestSermonWidget(),
+    BulletinWidget(),
+    ServiceTimesWidget(),
+    GivingWidget(),
+    EventsWidget(),
+    ContactCard(
+        title: 'Contact Grace Church',
+        address: '101 N 7th St\nAkron, PA 17501',
+        email: 'akrongrace@gmail.com',
+        phone: '717-859-2700',
+        color: Colors.white,
+        textColor: Colors.black
+    ),
+    ContactCard(
+        title: 'Contact Helping Hands Daycare',
+        address: '101 N 7th St\nAkron, PA 17501',
+        email: 'daycarehelpinghands@hotmal.com',
+        phone: '717-859-2032 x223',
+        color: Colors.black,
+        textColor: Colors.white
+    ),
+  ];
 
+  Home({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -35,49 +59,39 @@ class Home extends StatelessWidget {
       onTap: ()=> FocusScope.of(context).unfocus(),
       child: Scaffold (
         backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Image.asset('assets/akron-icon.webp'),
-          ),
-          title: const Text(
-              'Akron Grace EC Church',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              backgroundColor: const Color(0xFF011332),
+              foregroundColor: Colors.white,
+              expandedHeight: 170.0,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Image.asset('assets/akron-icon.webp'),
               ),
-          ),
-          actions: const <Widget>[
-            Padding(
-              padding: EdgeInsets.only(right: 10),
-              child: Icon(Icons.menu),
+              actions: const <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Icon(Icons.menu),
+                ),
+              ],
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text(
+                  'Akron Grace EC Church',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                background: Image.asset(
+                  'assets/church_outside.webp',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ],
-        ),
-        body: ListView(
-          children: const <Widget>[
-            WelcomeWidget(),
-            LatestSermonWidget(),
-            BulletinWidget(),
-            ServiceTimesWidget(),
-            GivingWidget(),
-            EventsWidget(),
-            ContactCard(
-              title: 'Contact Grace Church',
-              address: '101 N 7th St\nAkron, PA 17501',
-              email: 'akrongrace@gmail.com',
-              phone: '717-859-2700',
-              color: Colors.white,
-              textColor: Colors.black
-            ),
-            ContactCard(
-              title: 'Contact Helping Hands Daycare',
-              address: '101 N 7th St\nAkron, PA 17501',
-              email: 'daycarehelpinghands@hotmal.com',
-              phone: '717-859-2032 x223',
-              color: Colors.black,
-              textColor: Colors.white
+            SliverList(
+              delegate: SliverChildListDelegate(widgets),
             ),
           ],
         ),
